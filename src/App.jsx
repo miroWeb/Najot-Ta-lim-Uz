079fb511e16469e1e0ebf82e3f4fcebcd5833b2d
miroWeb <mirodiljs2004@gmail.com>
2024-01-11 18:11:00 +0500

    Najot Ta'lim

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import AccordionUsage from "./components/AccordionUsage";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/* <AccordionUsage/> */}
+    </div>
+  );
+}
+
+export default App;
